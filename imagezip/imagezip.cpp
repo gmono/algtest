19d075cd454a6837b2b57d5b512e18996e4f6c6d
高子健 <gaozijian0411@outlook.com>
2017-04-07 13:23:41 +0800

    异或还原视频处理测试成功

--- imagezip/imagezip.cpp
+++ imagezip/imagezip.cpp
@@ -7,10 +7,13 @@
 using namespace cv;
 
 #define calwsize(bsize,type) (bsize/sizeof(type)+(bsize%sizeof(type)!=0 ? 1:0))
-inline void imagezip(int rows,int cols, uchar *b1, uchar *b2, uchar *res)
+inline void imagezip(Mat &buf1, Mat &buf2, Mat &result)
 {
-	if (!(rows&&cols)) return;
-	int bufsize = (rows*cols)/sizeof(__m256i);
+	if (!(result.rows&&result.cols)) return;
+	uchar *b1 = buf1.data;
+	uchar *b2 = buf2.data;
+	uchar *res = result.data;
+	int bufsize = result.step*result.rows;
 	int i = 0;
 #pragma omp parallel for
 	for (i = 0; i<bufsize; i += sizeof(__m256i))
@@ -18,7 +21,22 @@ inline void imagezip(int rows,int cols, uchar *b1, uchar *b2, uchar *res)
 		*(__m256i *)(res + i) = _mm256_xor_si256(*(__m256i *)(b1 + i), *(__m256i *)(b2 + i));
 	}
 }
+template<class T>
+void g_imagezip(Mat &buf1,Mat &buf2,Mat &result)
+{
+	if (!(result.rows&&result.cols)) return;
+	uchar *b1 = buf1.data;
+	uchar *b2 = buf2.data;
+	uchar *res = result.data;
 
+	int bufsize = result.step*result.rows ;
+	int i = 0;
+#pragma omp parallel for
+	for (i = 0; i<bufsize; i += sizeof(T))
+	{
+		*(T *)(res + i) = *(T *)(b1 + i) ^ *(T *)(b2 + i);
+	}
+}
 bool stop = false;
 void stopfun(int state, void *userdata)
 {
@@ -34,23 +52,29 @@ int main()
 	namedWindow("myvideo", WINDOW_NORMAL);
 	namedWindow("myvideo2", WINDOW_NORMAL);
 	namedWindow("huanyuan", WINDOW_NORMAL);
-	int delay = 1000 / rate;
+	int delay = 1000 / rate/2;
 	capture.set(CV_CAP_PROP_POS_FRAMES, 100);
 	while (!stop)
 	{
 		frame.copyTo(old);
 		if (!capture.read(frame))
 			break;
-		Mat res(frame.rows,frame.cols,CV_8UC3);
-		Mat hy(frame.rows, frame.cols, CV_8UC3);
-		if(frame.data&&old.data)
-			imagezip(frame.rows, frame.step,frame.data,old.data,res.data);
 		imshow("myvideo", frame);
-		imshow("myvideo2", res);
+		Mat res(frame.rows, frame.cols, CV_8UC3);
+		Mat hy(frame.rows, frame.cols, CV_8UC3);
+		if (frame.data&&old.data)
+		{
+			imagezip(frame,old,res);
+			imshow("myvideo2", res);
+		}
+		
+		
 			//ÏÔÊ¾Òì»ò»Ø¹é
-		if(old.data&&res.data)
-			imagezip(frame.rows, frame.step, old.data, res.data, hy.data);
-		imshow("huanyuan", hy);
+		if (old.data&&res.data)
+		{
+			imagezip(old,res,hy);
+			imshow("huanyuan", hy);
+		}
 
 		waitKey(delay);
 	}
